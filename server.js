e432323fc406c50d2c2a6ec5c0b25bccdb723cf3
CAESAR <caesaradek@gmail.com>
2019-08-08 22:01:17 +0100

    Pushing to git

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ app.use(express.json());
 app.use("/api/items", shopRoutes);
 
 //serve static assets in production
+// app.use(express.static("client/build"));
+
+//     app.get("*", (req, res)=>{
+//         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//     });
 if(process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"));
 
